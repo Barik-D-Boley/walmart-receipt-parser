ee1ac13c702d6c67dfb52443b68fcb06b98dafe7
Barik-Boley <barik.boley@gmail.com>
2026-08-07 23:46:18 -0600

    Connected button to js file

--- extension/popup.js
+++ extension/popup.js
@@ -1,1 +1,8 @@
 // Executes when I click the button on popup.html
+const scrapeButton = document.getElementById("scrapeReceipt");
+
+function scrapeReceipt() {
+  alert("Hello World!");
+}
+
+scrapeButton.addEventListener("click", scrapeReceipt);
